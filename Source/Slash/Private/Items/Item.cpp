12dc3fbd0fc7b58961d547476e0ed0630502eaba
Calum Murray <calum.murray98@outlook.com>
2023-05-23 20:33:02 +0100

    Initial commit

---
+++ Source/Slash/Private/Items/Item.cpp
@@ -0,0 +1,32 @@
+#include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+}
