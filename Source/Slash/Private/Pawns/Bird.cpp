516fd55139ca9f6f7a67818161604d2323deca20
Calum Murray <calum.murray98@outlook.com>
2023-06-25 11:25:51 +0100

    Added legacy Input binding for MoveForward

--- Source/Slash/Private/Pawns/Bird.cpp
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -17,18 +17,23 @@ ABird::ABird()
 
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
+}
+
+void ABird::MoveForward(float Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), Value);
 }
 
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -36,5 +41,6 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ABird::MoveForward);
 }
 
--- Source/Slash/Public/Pawns/Bird.h
+++ Source/Slash/Public/Pawns/Bird.h
@@ -24,6 +24,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	void MoveForward(float Value);
 
 private:
 	UPROPERTY(VisibleAnywhere)
